7e62c0aad9c8ec9e0d228f05333cb403dd145239
Ugo Theveny <utheveny42@gmail.com>
2022-05-05 12:01:55 +0200

    save

--- srcs/requirements/wordpress/tools/wordpress_script.sh
+++ srcs/requirements/wordpress/tools/wordpress_script.sh
@@ -2,5 +2,4 @@
 mkdir -p var/www/
 wget https://fr.wordpress.org/latest-fr_FR.tar.gz -P /var/www
 tar xfC /var/www/latest-fr_FR.tar.gz /var/www/ && rm -rf /var/www/latest-fr_FR.tar.gz
-mv /tmp/wp-config.php /var/www/wordpress/
-chown -R www-data /var/www/wordpress
+mv /tmp/wp-config.php /var/www/wordpress/ && rm -rf /var/www/wordpress/wp-config-sample.php
