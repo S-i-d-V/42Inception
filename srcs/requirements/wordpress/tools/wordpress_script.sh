7ee475d6bcb2fd4e407f5b13c5c9b4e38866acc1
Ugo Theveny <utheveny42@gmail.com>
2022-05-10 21:46:46 +0200

    mysql from 0

--- srcs/requirements/wordpress/tools/wordpress_script.sh
+++ srcs/requirements/wordpress/tools/wordpress_script.sh
@@ -3,18 +3,17 @@ mkdir -p var/www/
 
 #Config php
 mkdir -p run/php
-rm -rf etc/php/7.3/fpm/pool.d/www.conf && mv /tmp/www.conf etc/php/7.3/fpm/pool.d/
 
 #Install wordpress
 wget https://fr.wordpress.org/latest-fr_FR.tar.gz -P /var/www
 tar xfC /var/www/latest-fr_FR.tar.gz /var/www/
 rm -rf /var/www/latest-fr_FR.tar.gz
-
-#Config wordpress
-mv /tmp/wp-config.php /var/www/wordpress/
-rm -rf /var/www/wordpress/wp-config-sample.php
+#Install wp-cli
+wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+sudo mv wp-cli.phar /usr/local/bin/wp
 
 #Start php fpm
 php-fpm7.3
 
-tail -f
+exec "$@"
